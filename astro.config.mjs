3103325bede9e0c0647cb9835411121d3b683778
WestinSherman <westin@arnst.ca>
2025-05-12 19:02:46 +0200

    Fixed usefulLinks file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,25 @@ import { defineConfig } from 'astro/config';
 import { CONFIG } from './src/config';
 import mdx from '@astrojs/mdx';
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
-    base: "/",
-    integrations: [mdx()],
-    content: {
-        collections: {
-            posts: {
-                schema: "src/content/config.ts#posts",
-            },
-            finds: {
-                schema: "src/content/config.ts#finds",
-            },
-            projects: {
-                schema: "src/content/config.ts#projects",
-            },
-        },
-    },
+  base: "/",
+  integrations: [mdx()],
+
+  content: {
+      collections: {
+          posts: {
+              schema: "src/content/config.ts#posts",
+          },
+          finds: {
+              schema: "src/content/config.ts#finds",
+          },
+          projects: {
+              schema: "src/content/config.ts#projects",
+          },
+      },
+  },
+
+  adapter: netlify(),
 });
