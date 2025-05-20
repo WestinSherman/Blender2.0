b40c8231f1a16b29598821121c7bda4a8502e998
WestinSherman <westin@arnst.ca>
2025-05-20 14:55:19 +0200

    Removed Ideas and Useful links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,26 @@
 import { defineConfig } from 'astro/config';
 import { CONFIG } from './src/config';
 import mdx from '@astrojs/mdx';
-
+import react from '@astrojs/react';
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  base: "/",
-  integrations: [mdx()],
+    base: "/",
+    integrations: [mdx(), react()],
 
-  content: {
-      collections: {
-          posts: {
-              schema: "src/content/config.ts#posts",
-          },
-          finds: {
-              schema: "src/content/config.ts#finds",
-          },
-          projects: {
-              schema: "src/content/config.ts#projects",
-          },
-      },
-  },
+    content: {
+        collections: {
+            posts: {
+                schema: "src/content/config.ts#posts",
+            },
+            finds: {
+                schema: "src/content/config.ts#finds",
+            },
+            projects: {
+                schema: "src/content/config.ts#projects",
+            },
+        },
+    },
 
-  adapter: netlify(),
+    adapter: netlify(),
 });
